9215fc2ae90627b6f5fbc565524996ee1b46c851
Sergey Nuzhdin <ipaq.lw@gmail.com>
2019-03-21 07:31:55 +0000

    Fix delayed cleanup logic. Fixes #20

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -6,7 +6,6 @@ import (
 	"reflect"
 	"regexp"
 	"strconv"
-	"sync"
 	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -18,11 +17,19 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
+const resyncPeriod = time.Second * 30
+
 // PodController watches the kubernetes api for changes to Pods and
 // delete completed Pods without specific annotation
 type PodController struct {
 	podInformer cache.SharedIndexInformer
 	kclient     *kubernetes.Clientset
+
+	keepSuccessHours float64
+	keepFailedHours  float64
+	keepPendingHours float64
+	dryRun           bool
+	isLegacySystem   bool
 }
 
 // CreatedByAnnotation type used to match pods created by job
@@ -39,41 +46,63 @@ type CreatedByAnnotation struct {
 	}
 }
 
-// NewPodController creates a new NewPodController
-func NewPodController(kclient *kubernetes.Clientset, opts map[string]string) *PodController {
-	podWatcher := &PodController{}
+func isLegacySystem(v version.Info) bool {
+	oldVersion := false
 
-	keepSuccessHours, _ := strconv.Atoi(opts["keepSuccessHours"])
-	keepFailedHours, _ := strconv.Atoi(opts["keepFailedHours"])
-	keepPendingHours, _ := strconv.Atoi(opts["keepPendingHours"])
-	dryRun, _ := strconv.ParseBool(opts["dryRun"])
-	version, err := kclient.ServerVersion()
+	major, _ := strconv.Atoi(v.Major)
 
-	if err != nil {
-		log.Fatalf("Failed to retrieve server version %v", err)
+	var minor int
+	re := regexp.MustCompile("[0-9]+")
+	m := re.FindAllString(v.Minor, 1)
+	if len(m) != 0 {
+		minor, _ = strconv.Atoi(m[0])
+	} else {
+		log.Printf("failed to parse minor version %s", v.Minor)
+		minor = 0
 	}
 
+	if major < 2 && minor < 8 {
+		oldVersion = true
+	}
+
+	return oldVersion
+}
+
+// NewPodController creates a new NewPodController
+func NewPodController(kclient *kubernetes.Clientset, namespace string, dryRun bool, opts map[string]float64) *PodController {
+	serverVersion, err := kclient.ServerVersion()
+	if err != nil {
+		log.Fatalf("Failed to retrieve server serverVersion %v", err)
+	}
+
+	podWatcher := &PodController{
+		keepSuccessHours: opts["keepSuccessHours"],
+		keepFailedHours:  opts["keepFailedHours"],
+		keepPendingHours: opts["keepPendingHours"],
+		dryRun:           dryRun,
+		isLegacySystem:   isLegacySystem(*serverVersion),
+	}
 	// Create informer for watching Namespaces
 	podInformer := cache.NewSharedIndexInformer(
 		&cache.ListWatch{
 			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
-				return kclient.CoreV1().Pods(opts["namespace"]).List(options)
+				return kclient.CoreV1().Pods(namespace).List(options)
 			},
 			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
-				return kclient.CoreV1().Pods(opts["namespace"]).Watch(options)
+				return kclient.CoreV1().Pods(namespace).Watch(options)
 			},
 		},
 		&v1.Pod{},
-		time.Second*30,
+		resyncPeriod,
 		cache.Indexers{},
 	)
 	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(cur interface{}) {
-			podWatcher.doTheMagic(cur, keepSuccessHours, keepFailedHours, keepPendingHours, dryRun, *version)
+		AddFunc: func(obj interface{}) {
+			podWatcher.Process(obj)
 		},
-		UpdateFunc: func(old, cur interface{}) {
-			if !reflect.DeepEqual(old, cur) {
-				podWatcher.doTheMagic(cur, keepSuccessHours, keepFailedHours, keepPendingHours, dryRun, *version)
+		UpdateFunc: func(old, new interface{}) {
+			if !reflect.DeepEqual(old, new) {
+				podWatcher.Process(new)
 			}
 		},
 	})
@@ -84,46 +113,46 @@ func NewPodController(kclient *kubernetes.Clientset, opts map[string]string) *Po
 	return podWatcher
 }
 
+func (c *PodController) periodicCacheCheck() {
+	for {
+		for _, obj := range c.podInformer.GetStore().List() {
+			c.Process(obj)
+		}
+		time.Sleep(2 * resyncPeriod)
+	}
+}
+
 // Run starts the process for listening for pod changes and acting upon those changes.
-func (c *PodController) Run(stopCh <-chan struct{}, wg *sync.WaitGroup) {
+func (c *PodController) Run(stopCh <-chan struct{}) {
 	log.Printf("Listening for changes...")
-	// When this function completes, mark the go function as done
-	defer wg.Done()
 
-	// Increment wait group as we're about to execute a go function
-	wg.Add(1)
-
-	// Execute go function
 	go c.podInformer.Run(stopCh)
+	go c.periodicCacheCheck()
 
-	// Wait till we receive a stop signal
 	<-stopCh
 }
 
-func (c *PodController) doTheMagic(cur interface{}, keepSuccessHours int, keepFailedHours int, keepPendingHours int, dryRun bool, version version.Info) {
-
-	podObj := cur.(*v1.Pod)
-	parentJobName := c.getParentJobName(podObj, version)
+func (c *PodController) Process(obj interface{}) {
+	podObj := obj.(*v1.Pod)
+	parentJobName := c.getParentJobName(podObj)
 	// if we couldn't find a prent job name, ignore this pod
 	if parentJobName == "" {
-		log.Printf("Pod %s was not created by a job... ignoring", podObj.Name)
 		return
 	}
 
 	executionTimeHours := c.getExecutionTimeHours(podObj)
-	log.Printf("Checking pod %s with %s status that was executed %f hours ago", podObj.Name, podObj.Status.Phase, executionTimeHours)
 	switch podObj.Status.Phase {
 	case v1.PodSucceeded:
-		if keepSuccessHours == 0 || (keepSuccessHours > 0 && executionTimeHours > float32(keepSuccessHours)) {
-			c.deleteObjects(podObj, parentJobName, dryRun)
+		if c.keepSuccessHours == 0 || (c.keepSuccessHours > 0 && executionTimeHours > c.keepSuccessHours) {
+			c.deleteObjects(podObj, parentJobName)
 		}
 	case v1.PodFailed:
-		if keepFailedHours == 0 || (keepFailedHours > 0 && executionTimeHours > float32(keepFailedHours)) {
-			c.deleteObjects(podObj, parentJobName, dryRun)
+		if c.keepFailedHours == 0 || (c.keepFailedHours > 0 && executionTimeHours > c.keepFailedHours) {
+			c.deleteObjects(podObj, parentJobName)
 		}
 	case v1.PodPending:
-		if keepPendingHours > 0 && executionTimeHours > float32(keepPendingHours) {
-			c.deleteObjects(podObj, parentJobName, dryRun)
+		if c.keepPendingHours > 0 && executionTimeHours > c.keepPendingHours {
+			c.deleteObjects(podObj, parentJobName)
 		}
 	default:
 		return
@@ -131,65 +160,53 @@ func (c *PodController) doTheMagic(cur interface{}, keepSuccessHours int, keepFa
 }
 
 // method to calculate the hours that passed since the pod's execution end time
-func (c *PodController) getExecutionTimeHours(podObj *v1.Pod) (executionTimeHours float32) {
-	executionTimeHours = 0.0
-	currentUnixTime := time.Now().Unix()
-	podConditions := podObj.Status.Conditions
-	var pc v1.PodCondition
-	for _, pc = range podConditions {
+func (c *PodController) getExecutionTimeHours(podObj *v1.Pod) float64 {
+	currentUnixTime := time.Now()
+	for _, pc := range podObj.Status.Conditions {
 		// Looking for the time when pod's condition "Ready" became "false" (equals end of execution)
 		if pc.Type == v1.PodReady && pc.Status == v1.ConditionFalse {
-			executionTimeUnix := pc.LastTransitionTime.Unix()
-			executionTimeHours = (float32(currentUnixTime) - float32(executionTimeUnix)) / float32(3600)
+			return currentUnixTime.Sub(pc.LastTransitionTime.Time).Hours()
 		}
 	}
 
-	return
+	return 0.0
 }
 
-func (c *PodController) deleteObjects(podObj *v1.Pod, parentJobName string, dryRun bool) {
+func (c *PodController) deleteObjects(podObj *v1.Pod, parentJobName string) {
 	// Delete Pod
-	if !dryRun {
+	if !c.dryRun {
 		log.Printf("Deleting pod '%s'", podObj.Name)
 		var po metav1.DeleteOptions
-		c.kclient.CoreV1().Pods(podObj.Namespace).Delete(podObj.Name, &po)
+		err := c.kclient.CoreV1().Pods(podObj.Namespace).Delete(podObj.Name, &po)
+		if err != nil {
+			log.Printf("failed to delete job %s: %v", parentJobName, err)
+		}
 	} else {
 		log.Printf("dry-run: Pod '%s' would have been deleted", podObj.Name)
 	}
 	// Delete Job itself
-	if !dryRun {
+	if !c.dryRun {
 		log.Printf("Deleting job '%s'", parentJobName)
 		var jo metav1.DeleteOptions
-		c.kclient.BatchV1Client.Jobs(podObj.Namespace).Delete(parentJobName, &jo)
+		err := c.kclient.BatchV1Client.Jobs(podObj.Namespace).Delete(parentJobName, &jo)
+		if err != nil {
+			log.Printf("failed to delete job %s: %v", parentJobName, err)
+		}
 	} else {
 		log.Printf("dry-run: Job '%s' would have been deleted", parentJobName)
 	}
 	return
 }
 
-func (c *PodController) getParentJobName(podObj *v1.Pod, version version.Info) (parentJobName string) {
+func (c *PodController) getParentJobName(podObj *v1.Pod) (parentJobName string) {
 
-	oldVersion := false
-
-	major, _ := strconv.Atoi(version.Major)
-
-	var minor int
-	re := regexp.MustCompile("[0-9]+")
-	m := re.FindAllString(version.Minor, 1)
-	if len(m) != 0 {
-		minor, _ = strconv.Atoi(m[0])
-	} else {
-		log.Printf("failed to parse minor version %s", version.Minor)
-		minor = 0
-	}
-
-	if major < 2 && minor < 8 {
-		oldVersion = true
-	}
-
-	if oldVersion {
+	if c.isLegacySystem {
 		var createdMeta CreatedByAnnotation
-		json.Unmarshal([]byte(podObj.ObjectMeta.Annotations["kubernetes.io/created-by"]), &createdMeta)
+		err := json.Unmarshal([]byte(podObj.ObjectMeta.Annotations["kubernetes.io/created-by"]), &createdMeta)
+		if err != nil {
+			log.Printf("failed to unmarshal annotations for pod %s. %v", podObj.Name, err)
+			return
+		}
 		if createdMeta.Reference.Kind == "Job" {
 			parentJobName = createdMeta.Reference.Name
 		}
