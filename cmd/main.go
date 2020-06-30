lwolf/kube-cleanup-operator

commit 7e342c16f94d6e8aec1a0a7811b210b1bcf14e6b

don't delete jobs and pods owner by cronjobs (#50)

cmd/main.go
@@ -146,16 +146,13 @@ func main() {
 	wg.Add(1)
 	go func() {
 		for {
-			select {
-			case <-stopCh:
-				log.Println("shutting http server down")
-				err := server.Shutdown(ctx)
-				if err != nil {
-					log.Printf("failed to shutdown metrics server: %v\n", err)
-				}
-				wg.Done()
-				return
+			<-stopCh
+			log.Println("shutting http server down")
+			err := server.Shutdown(ctx)
+			if err != nil {
+				log.Printf("failed to shutdown metrics server: %v\n", err)
 			}
+			wg.Done()
 		}
 	}()
 
@@ -168,7 +165,7 @@ func main() {
 func newClientSet(runOutsideCluster bool) (*kubernetes.Clientset, error) {
 	kubeConfigLocation := ""
 
-	if runOutsideCluster == true {
+	if runOutsideCluster {
 		if os.Getenv("KUBECONFIG") != "" {
 			kubeConfigLocation = filepath.Join(os.Getenv("KUBECONFIG"))
 		} else {

pkg/controller/controller.go
@@ -160,13 +160,18 @@ func (c *Kleaner) Process(obj interface{}) {
 			return
 		}
 
+		owners := getJobOwnerKinds(job)
+		if isOwnedByCronJob(owners) {
+			return
+		}
+
 		finishTime := extractJobFinishTime(job)
 
 		if finishTime.IsZero() {
 			return
 		}
 
-		timeSinceFinish := time.Now().Sub(finishTime)
+		timeSinceFinish := time.Since(finishTime)
 
 		if job.Status.Succeeded > 0 {
 			if c.deleteSuccessfulAfter > 0 && timeSinceFinish > c.deleteSuccessfulAfter {
@@ -190,7 +195,7 @@ func (c *Kleaner) Process(obj interface{}) {
 		if podFinishTime.IsZero() {
 			return
 		}
-		age := time.Now().Sub(podFinishTime)
+		age := time.Since(podFinishTime)
 		// orphaned pod: those that do not have any owner references
 		// - uses c.deleteOrphanedAfter
 		if len(owners) == 0 {
@@ -202,6 +207,14 @@ func (c *Kleaner) Process(obj interface{}) {
 		// owned by job, have exactly one ownerReference present and its kind is Job
 		//  - uses the c.deleteSuccessfulAfter, c.deleteFailedAfter, c.deletePendingAfter
 		if isOwnedByJob(owners) {
+			jobOwnerName := pod.OwnerReferences[0].Name
+			jobOwner, exists, err := c.jobInformer.GetStore().GetByKey(pod.Namespace + "/" + jobOwnerName)
+			if err != nil {
+				log.Printf("Can't find job '%s:%s`", pod.Namespace, jobOwnerName)
+
+			} else if exists && isOwnedByCronJob(getJobOwnerKinds(jobOwner.(*batchv1.Job))) {
+				return
+			}
 			toDelete := c.maybeDeletePod(pod.Status.Phase, age)
 			if toDelete {
 				c.deletePods(pod)
@@ -275,10 +288,27 @@ func getPodOwnerKinds(pod *corev1.Pod) []string {
 	return kinds
 }
 
+func getJobOwnerKinds(job *batchv1.Job) []string {
+	var kinds []string
+	for _, ow := range job.OwnerReferences {
+		kinds = append(kinds, ow.Kind)
+	}
+	return kinds
+}
+
 // isOwnedByJob returns true if and only if pod has a single owner
 // and this owners kind is Job
 func isOwnedByJob(ownerKinds []string) bool {
 	if len(ownerKinds) == 1 && ownerKinds[0] == "Job" {
+		return true
+	}
+	return false
+}
+
+// isOwnedByCronJob returns true if and only if job has a single owner CronJob
+// and this owners kind is CronJob
+func isOwnedByCronJob(ownerKinds []string) bool {
+	if len(ownerKinds) == 1 && ownerKinds[0] == "CronJob" {
 		return true
 	}
 	return false

pkg/controller/controller_legacy.go
@@ -37,13 +37,13 @@ type PodController struct {
 // CreatedByAnnotation type used to match pods created by job
 type CreatedByAnnotation struct {
 	Kind       string
-	ApiVersion string
+	APIVersion string
 	Reference  struct {
 		Kind            string
 		Namespace       string
 		Name            string
-		Uid             string
-		ApiVersion      string
+		UID             string
+		APIVersion      string
 		ResourceVersion string
 	}
 }
@@ -210,7 +210,6 @@ func (c *PodController) deleteObjects(podObj *corev1.Pod, parentJobName string) 
 	} else {
 		log.Printf("dry-run: Pod '%s' would have been deleted", podObj.Name)
 	}
-	return
 }
 
 func (c *PodController) getParentJobName(podObj *corev1.Pod) (parentJobName string) {

commit 1521444c0321109bd61514d672459eb2ef98e2b6

Fix panic introduced in #50
Commit 8bc42b1bd07aae9e39ff40607e67053e04dd916b introduced a panic during
shutdown loop - `panic: sync: negative WaitGroup counter`.

Signed-off-by: Sergey Nuzhdin <ipaq.lw@gmail.com>

cmd/main.go
@@ -145,6 +145,7 @@ func main() {
 
 	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		for {
 			<-stopCh
 			log.Println("shutting http server down")
@@ -152,7 +153,7 @@ func main() {
 			if err != nil {
 				log.Printf("failed to shutdown metrics server: %v\n", err)
 			}
-			wg.Done()
+			break
 		}
 	}()
 